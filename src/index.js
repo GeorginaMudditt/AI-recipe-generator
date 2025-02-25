3be4cbf1e6394bdba7bcc80a35fb688bb7df5fad
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-02-25 16:42:43 +0100

    add typewriter functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Recipe example",
+    loop: false,
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+
+recipeFormElement.addEventListener("submit", generateRecipe);
